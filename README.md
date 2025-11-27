df9220619a9be605285aa8ef1a0be508904b9ec0
mac010ucsd <97704471+mac010ucsd@users.noreply.github.com>
2025-11-26 18:37:45 -0800

    complete mac tile

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # systolic_array
+todo
+- [x] mac tile 2-4 bit switchable
+- [x] mac tile tb
+- [ ] mac row
+- [ ] mac row tb
+- [ ] mac array
+- [ ] mac array tb
+- [ ] etc
--- mac_tile.v
+++ mac_tile.v
@@ -1,9 +1,11 @@
 // Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
 // Please do not spread this code without permission 
-module mac_tile (clk, out_s0, out_s1, in_w0, in_w1, out_e0, out_e1, in_n0, in_n1, inst_w, inst_e, reset);
+module mac_tile (clk, 
+	out_s0, out_s1, in_w0, in_w1, out_e0, out_e1, in_n0, in_n1, 
+	inst_w, inst_e, reset);
 
 parameter bw = 2;
-parameter psum_bw = 11;
+parameter psum_bw = 9;
 
 // dual psum output, dual 2bit input moving act, etc.
 input  [psum_bw-1:0] in_n0;
@@ -19,8 +21,8 @@ output [bw-1:0] out_e0;
 output [bw-1:0] out_e1; 
 
 // 2 bit kernel loading instruction, 1 bit execution, 1 bit mode?
-input  [3:0] inst_w;
-output [3:0] inst_e;
+input  [2:0] inst_w;
+output [2:0] inst_e;
 // mode will always propagate, execution will always propagate,
 // kernel loading depends.
 
@@ -29,8 +31,8 @@ input  reset;
 
 reg [bw-1:0] a_q0;
 reg [bw-1:0] a_q1;
-reg [bw-1:0] b_q0;
-reg [bw-1:0] b_q1;
+reg [4-1:0] b_q0;
+reg [4-1:0] b_q1;
 reg [psum_bw-1:0] c_q0;
 reg [psum_bw-1:0] c_q1;
 reg [2:0] inst_q;
@@ -47,32 +49,30 @@ assign inst_e[2:0] = inst_q[2:0];
 
 // inst_e[0] = left or right reg
 
-// inst_e[2] = mode
+// inst_e[2] = mode. 1 = 4bit mode, 0 = 2bit mode
 // inst_e[1] = exec
 // inst_e[0] = weightload
 
-/*
-todo
-assign out_s[psum_bw-1:0] = mac_output[psum_bw-1:0];
-assign out_s[psum_bw-1:0] = mac_output[psum_bw-1:0];
-*/
-
 wire [10:0] big_psum;
-assign big_psum = mac_output0 + (mac_output1 << 2);
+// wire [1:0] lsb_sign;
+// assign lsb_sign = {mac_output0[8], mac_output0[8]};
+assign big_psum = {{2{mac_output0[8]}}, mac_output0} 
+	+ {mac_output1, 2'b0};
 
 // assign from big psum if STORED mode is 1 else just pull from mac output
 assign out_s0 = inst_q[2] ? {1'b0, big_psum[7:0]} : mac_output0[psum_bw-1:0];
+//assign out_s1 = inst_q[2] ? {2'b0, big_psum[10:8], 4'b0} : mac_output1[psum_bw-1:0];
 assign out_s1 = inst_q[2] ? {big_psum[10:8], 6'b0} : mac_output1[psum_bw-1:0];
 
 
-mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance0 (
+mac #(.a_bw(bw), .b_bw(4), .psum_bw(psum_bw)) mac_instance0 (
         .a(a_q0), 
         .b(b_q0),
         .c(c_q0),
 	.out(mac_output0)
 ); 
 
-mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance1 (
+mac #(.a_bw(bw), .b_bw(4), .psum_bw(psum_bw)) mac_instance1 (
         .a(a_q1), 
         .b(b_q1),
         .c(c_q1),
@@ -82,6 +82,7 @@ mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance1 (
 always@(posedge clk) begin
 	
 	if(reset==1'b1)	begin
+		// i feel like reset should clear everything...
 		inst_q <= 0;
 		load_ready_q <= 2'b11; //weight not loaded yet
 	end
